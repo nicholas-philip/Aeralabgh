009a8dd8c8ef4012ec255909e5b86f613de822c9
Isaac Opoku <isaacmanuopoku@gmail.com>
2024-08-27 23:54:44 +0000

    light and dark theme feature  added

--- script/app.js
+++ script/app.js
@@ -51,3 +51,52 @@ getExitNavMenu.addEventListener('click', () => {
   };
 
 });
+
+
+// light and dark theme toggle
+
+
+const getThemeToggle = document.getElementsByClassName('theme-btn');
+
+// get document body
+const body = document.body;
+
+// getting theme status in localStorage
+const savedLocalStorage = localStorage.getItem('theme', 'light');
+
+// localStorage to check if the theme is dark
+if (savedLocalStorage === 'light') {
+  body.classList.add('light-theme');
+  document.querySelector('#nav').classList.add('light-theme');
+  document.querySelector('#sm-navbar').classList.add('light-theme');
+} else {
+  body.classList.add('dark-theme');
+  document.querySelector('#nav').classList.add('dark-theme');
+  document.querySelector('#sm-navbar').classList.add('dark-theme');
+}
+
+for (let i = 0; i < getThemeToggle.length; i++) {
+  
+  getThemeToggle[i].addEventListener('click', () => {
+    
+    if (body.classList.contains('dark-theme')) {
+      body.classList.remove('dark-theme');
+      body.classList.add('light-theme');
+      document.querySelector('#nav').classList.remove('dark-theme');
+      document.querySelector('#nav').classList.add('light-theme');
+      document.querySelector('#sm-navbar').classList.remove('dark-theme');
+      document.querySelector('#sm-navbar').classList.add('light-theme');
+      localStorage.setItem('theme', 'light');
+    } else {
+      body.classList.remove('light-theme');
+      body.classList.add('dark-theme');
+      document.querySelector('#nav').classList.remove('light-theme');
+      document.querySelector('#nav').classList.add('dark-theme');
+      document.querySelector('#sm-navbar').classList.remove('light-theme');
+      document.querySelector('#sm-navbar').classList.add('dark-theme');
+      localStorage.setItem('theme', 'dark');
+    }
+
+  });
+  
+}
